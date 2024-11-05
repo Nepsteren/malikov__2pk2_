77d36914a291562e1023a4852055979ab1a0c0b6
Malikov Egor <egor122malikov@gmail.com>
2024-11-05 12:28:10 +0300

    first commit

---
+++ 666666666/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _666666666
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
